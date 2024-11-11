5274c50aba63c6f5c2989565c0240a7507e6928d
Emilien <4016501+unixfox@users.noreply.github.com>
2024-11-11 18:07:22 +0100

    fixing type returned for LineStopAreaSchedules

--- src/api/v3/lineStopAreaSchedulesGet.ts
+++ src/api/v3/lineStopAreaSchedulesGet.ts
@@ -1,8 +1,8 @@
-import { StopAreaSchedules } from "../../model/v3/stopAreaSchedules.ts";
+import { LineStopAreaSchedules } from "../../model/v3/stopAreaSchedules.ts";
 import { DEV_BASE_URL, PROD_BASE_URL, V3_GET_LINE_STOP_AREA_SCHEDULES } from "../../utils/endpoints.ts";
 import { ISFetcher } from "../../utils/fetcher.ts";
 
-export const getLineStopAreaSchedulesV3 = async (networkID: number, lineID:string, stopAreaID: string, useDevInstance=false): Promise<StopAreaSchedules> => {
+export const getLineStopAreaSchedulesV3 = async (networkID: number, lineID:string, stopAreaID: string, useDevInstance=false): Promise<LineStopAreaSchedules> => {
   const response = await ISFetcher(useDevInstance ? DEV_BASE_URL : PROD_BASE_URL + V3_GET_LINE_STOP_AREA_SCHEDULES(networkID,lineID,stopAreaID), {
     method: "GET",
     headers: {
@@ -12,5 +12,5 @@ export const getLineStopAreaSchedulesV3 = async (networkID: number, lineID:strin
   
   const data = await response.json();
 
-  return data as StopAreaSchedules;
+  return data as LineStopAreaSchedules;
 };
